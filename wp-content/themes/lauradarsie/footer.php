575ce97e645ed76fb8e9b86116c1d8934dc15572
mauro.fioravanzi <mauro.fioravanzi@mondadori.it>
2019-07-10 01:46:26 +0200

    no sidebar

--- wp-content/themes/lauradarsie/footer.php
+++ wp-content/themes/lauradarsie/footer.php
@@ -15,17 +15,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'lauradarsie' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'lauradarsie' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'lauradarsie' ), 'lauradarsie', '<a href="http://imeuro.io">Mauro Fioravanzi</a>' );
-				?>
+			<small>Copyright 2019 - Laura Darsié - Tutti i diritti riservati</small>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
