b6bab2ced61a660d6cc052d06cc6bba685b0ba47
mauro.fioravanzi <mauro.fioravanzi@mondadori.it>
2019-07-11 00:26:52 +0200

    blocchi sotto home

---
+++ wp-content/themes/lauradarsie/home.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package darsie
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+            <section id="latest-events">
+                <h3>Eventi</h3>
+                <?php
+                $args_events = array(
+                    'numberposts' => 3,
+                    'offset' => 0,
+                    'category' => 0,
+                    'orderby' => 'post_date',
+                    'order' => 'DESC',
+                    'post_type' => 'tribe_events',
+                    'post_status' => 'publish',
+                    'suppress_filters' => true
+                );
+                $recent_events = wp_get_recent_posts( $args_events, ARRAY_A );
+                ?>
+                <ul class="recent-posts recent-events">
+                <?php
+                foreach( $recent_events as $recent ){
+                    echo '<li class="recent-item"><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a> </li> ';
+                }
+                wp_reset_query();
+                ?>
+                </ul>
+            </section>
+
+            <section id="latest-posts">
+                <h3>Pubblicazioni</h3>
+                <?php
+                $args_posts = array(
+                    'numberposts' => 3,
+                    'offset' => 0,
+                    'category' => 'pubblicazioni',
+                    'orderby' => 'post_date',
+                    'order' => 'DESC',
+                    'post_type' => 'post',
+                    'post_status' => 'publish',
+                    'suppress_filters' => true
+                );
+                $recent_posts = wp_get_recent_posts( $args_posts, ARRAY_A );
+                ?>
+                <ul class="recent-posts recent-events">
+                <?php
+                foreach( $recent_posts as $recent ){
+                    echo '<li class="recent-item"><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a> </li> ';
+                }
+                wp_reset_query();
+                ?>
+                </ul>
+            </section>
+            
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+// get_sidebar();
+get_footer();
