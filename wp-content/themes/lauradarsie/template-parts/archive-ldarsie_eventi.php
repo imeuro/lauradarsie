a178cc117e63e3be7568b70b1bdc92883470afdd
imeuro <mauro.fioravanzi@gmail.com>
2019-08-21 00:04:17 +0200

    eventi

--- wp-content/themes/lauradarsie/template-parts/archive-ldarsie_eventi.php
+++ wp-content/themes/lauradarsie/template-parts/archive-ldarsie_eventi.php
@@ -9,29 +9,30 @@
 
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class('archive'); ?> style="background-image:url('<?php echo get_the_post_thumbnail_url() ?>');">
-    <!-- <figure class="entry-figure">
-        <?php // lauradarsie_post_thumbnail(); ?>
-    </figure> -->
 
-    <div class="entry-txt">
+    <a href="' . esc_url( get_permalink() ) . '" rel="bookmark">
 
-        <div class="entry-content">
-            <?php
-            $startdate = '';
-            if (get_field('evento_data_inizio')) {
-                $startdate = '<span class="ldarsie_eventdate">'.get_field('evento_data_inizio');
-                echo '</span>';
-            }
+        <div class="entry-txt">
+            <div class="entry-content">
+                <?php
+                $startdate = '';
+                if (get_field('evento_data_inizio')) {
+                    $startdate = '<span class="ldarsie_eventdate">'.get_field('evento_data_inizio');
+                    echo '</span>';
+                }
 
-            the_title( '<h2 class="entry-title">'.$startdate.' - <a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+                the_title( '<h2 class="entry-title">'.$startdate.' - ', '</h2>' );
 
-            echo '<p>'.wp_filter_nohtml_kses(get_the_content()).'</p>';
+                echo '<p>'.wp_filter_nohtml_kses(get_the_content()).'</p>';
 
-            ?>
-        </div><!-- .entry-content -->
-    </div>
+                ?>
+            </div><!-- .entry-content -->
+        </div>
 
-	<footer class="entry-footer">
-		<?php lauradarsie_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+        <footer class="entry-footer">
+            <?php lauradarsie_entry_footer(); ?>
+        </footer><!-- .entry-footer -->
+
+    </a>
+    
 </article><!-- #post-<?php the_ID(); ?> -->
