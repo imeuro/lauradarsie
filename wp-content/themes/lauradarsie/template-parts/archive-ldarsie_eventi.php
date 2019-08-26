8d22b4328d3a9cf19448b9f47615d5c047351ccc
mauro.fioravanzi <mauro.fioravanzi@mondadori.it>
2019-08-26 18:26:47 +0200

    fix links su blocchi archivio

--- wp-content/themes/lauradarsie/template-parts/archive-ldarsie_eventi.php
+++ wp-content/themes/lauradarsie/template-parts/archive-ldarsie_eventi.php
@@ -16,25 +16,22 @@ if (!$pthumb) {
 }
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class('archive '.$pclass); ?> style="background-image:url('<?php echo $pthumb ?>');">
+    <a href="<?php echo esc_url( get_permalink() ); ?>">
+        <div class="entry-txt">
 
-    <div class="entry-txt">
+            <div class="entry-content">
+                <?php
+                $startdate = '';
+                if (get_field('evento_data_inizio')) {
+                    $startdate = '<span class="ldarsie_eventdate">'.get_field('evento_data_inizio').'</span>';
+                }
 
-        <div class="entry-content">
-            <?php
-            $startdate = '';
-            if (get_field('evento_data_inizio')) {
-                $startdate = '<span class="ldarsie_eventdate">'.get_field('evento_data_inizio').'</span>';
-            }
+                the_title( '<h2 class="entry-title">'.$startdate.'<span class="tit-txt">', '</span></h2>' );
 
-            the_title( '<h2 class="entry-title">'.$startdate.'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+                echo '<p>'.wp_filter_nohtml_kses(get_the_content()).'</p>';
 
-            echo '<p>'.wp_filter_nohtml_kses(get_the_content()).'</p>';
-
-            ?>
-        </div><!-- .entry-content -->
-    </div>
-
-	<footer class="entry-footer">
-		<?php lauradarsie_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+                ?>
+            </div><!-- .entry-content -->
+        </div>
+</a>
 </article><!-- #post-<?php the_ID(); ?> -->
