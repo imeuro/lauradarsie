f2652db12c2523fb3c672d4c79cd526572374c77
mauro.fioravanzi <mauro.fioravanzi@mondadori.it>
2019-08-26 18:13:08 +0200

    idem

--- wp-content/themes/lauradarsie/template-parts/archive.php
+++ wp-content/themes/lauradarsie/template-parts/archive.php
@@ -15,21 +15,15 @@ if (!$pthumb) {
     $pclass = 'nopic';
 }
 ?>
-<a href="<?php echo esc_url( get_permalink() ); ?>">
 <article id="post-<?php the_ID(); ?>" <?php post_class('archive '.$pclass); ?> style="background-image:url('<?php echo $pthumb ?>');">
-
-    <div class="entry-txt">
-    
-        <div class="entry-content">
-            <?php
-            the_title( '<h2 class="entry-title">', '</h2>' );
-            echo '<p>'.wp_filter_nohtml_kses(get_the_content()).'</p>';
-            ?>
-        </div><!-- .entry-content -->
-    </div>
-
-	<footer class="entry-footer">
-		<?php // lauradarsie_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+    <a href="<?php echo esc_url( get_permalink() ); ?>">
+        <div class="entry-txt">
+            <div class="entry-content">
+                <?php
+                the_title( '<h2 class="entry-title">', '</h2>' );
+                echo '<p>'.wp_filter_nohtml_kses(get_the_content()).'</p>';
+                ?>
+            </div><!-- .entry-content -->
+        </div>
+    </a>
 </article><!-- #post-<?php the_ID(); ?> -->
-</a>
