1d0ac5efa36a102a21d5423aa184776e0eecb065
mauro.fioravanzi <mauro.fioravanzi@mondadori.it>
2019-09-04 18:27:08 +0200

    custom tax per eventi

--- wp-content/themes/lauradarsie/inc/post-types.php
+++ wp-content/themes/lauradarsie/inc/post-types.php
@@ -36,7 +36,7 @@ function ldarsie_CPT() {
 		'description'           => __( 'Calendario eventi', 'text_domain' ),
 		'labels'                => $labels,
 		'supports'              => array( 'title', 'editor', 'thumbnail', 'custom-fields' ),
-		'taxonomies'            => array( 'category' ),
+		'taxonomies'            => array( 'ldarsie_tipo_evento' ),
 		// 'taxonomies'            => array( 'category', 'post_tag' ),
 		'hierarchical'          => false,
 		'public'                => true,
@@ -90,4 +90,45 @@ add_action('pre_get_posts', 'ldarsie_pre_get_posts');
 
 
 
+
+
+
+// Register Custom Taxonomy
+function ldarsie_CT() {
+
+	$labels = array(
+		'name'                       => _x( 'Tipi evento', 'Taxonomy General Name', 'text_domain' ),
+		'singular_name'              => _x( 'Tipo evento', 'Taxonomy Singular Name', 'text_domain' ),
+		'menu_name'                  => __( 'Tipo evento', 'text_domain' ),
+		'all_items'                  => __( 'All Items', 'text_domain' ),
+		'parent_item'                => __( 'Parent Item', 'text_domain' ),
+		'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
+		'new_item_name'              => __( 'New Item Name', 'text_domain' ),
+		'add_new_item'               => __( 'Add New Item', 'text_domain' ),
+		'edit_item'                  => __( 'Edit Item', 'text_domain' ),
+		'update_item'                => __( 'Update Item', 'text_domain' ),
+		'view_item'                  => __( 'View Item', 'text_domain' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+		'popular_items'              => __( 'Popular Items', 'text_domain' ),
+		'search_items'               => __( 'Search Items', 'text_domain' ),
+		'not_found'                  => __( 'Not Found', 'text_domain' ),
+		'no_terms'                   => __( 'No items', 'text_domain' ),
+		'items_list'                 => __( 'Items list', 'text_domain' ),
+		'items_list_navigation'      => __( 'Items list navigation', 'text_domain' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => false,
+		'show_tagcloud'              => false,
+	);
+	register_taxonomy( 'ldarsie_tipo_evento', array( 'ldarsie_eventi' ), $args );
+
+}
+add_action( 'init', 'ldarsie_CT', 0 );
 ?>
