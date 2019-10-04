9ec18714d49262f73525c564c86039eb5b18e5aa
mauro.fioravanzi <mauro.fioravanzi@mondadori.it>
2019-10-04 14:46:17 +0200

    codice analytics

--- wp-content/themes/lauradarsie/footer.php
+++ wp-content/themes/lauradarsie/footer.php
@@ -23,5 +23,14 @@
 
 <?php wp_footer(); ?>
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-149336414-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-149336414-1');
+</script>
 </body>
 </html>
