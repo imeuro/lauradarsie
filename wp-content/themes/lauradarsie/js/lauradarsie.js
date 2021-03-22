edd988abd89d668d13e9a23a25403628adf6b04f
imeuro <mauro.fioravanzi@gmail.com>
2021-03-22 17:52:33 +0100

    fixed-bottom mobile

--- wp-content/themes/lauradarsie/js/lauradarsie.js
+++ wp-content/themes/lauradarsie/js/lauradarsie.js
@@ -16,17 +16,40 @@ window.addEventListener('scroll', function(){
 console.log("%c October 2019 - Laura Darsié - lauradarsie.it ",'background:#5285a7;color:#acc4cc');
 console.log("%c Powered by: Mauro Fioravanzi ",'background:#5285a7;color:#acc4cc');
 
-// lightbox covid&gravidanza
-var promoproj = 'gigi';
-let promoboxes = document.querySelectorAll('.promo-iniziativa');
-Array.from(promoboxes).forEach(function(el){
-    el.onclick = () => {
-        promoproj = basicLightbox.create(`
-            <div id="lightboxCont">
-                <a class="closebtn" onclick="javascript:document.querySelector('.basicLightbox.basicLightbox--visible').click();">CHIUDI</a>
-                <h3 style="text-align: center;font-size: 38px;max-width: 750px;margin: 0 auto;">Un percorso psicomusicale per affrontare con serenità il desiderio di maternità nell'epoca del coronavirus</h3>
-                <p><img src="https://www.lauradarsie.it/pub/desiderare-un-figlio.jpg" width="100%" /></p>
-            </div>
-        `).show()
+
+document.addEventListener('DOMContentLoaded', function(){
+    // lightbox covid&gravidanza
+    var promoproj = 'gigi';
+    let promoboxes = document.querySelectorAll('.promo-iniziativa');
+
+    Array.from(promoboxes).forEach(function(el){
+        el.onclick = () => {
+            promoproj = basicLightbox.create(`
+                <div id="lightboxCont">
+                    <a class="closebtn" onclick="javascript:document.querySelector('.basicLightbox.basicLightbox--visible').click();">CHIUDI</a>
+                    <h3 style="text-align: center;font-size: 38px;max-width: 750px;margin: 0 auto;">Un percorso psicomusicale per affrontare con serenità il desiderio di maternità nell'epoca del coronavirus</h3>
+                    <p><img src="https://www.lauradarsie.it/pub/desiderare-un-figlio.jpg" width="100%" /></p>
+                </div>
+            `).show()
+        }
+    });
+
+    if (document.body.clientWidth < 600) {
+        //promoboxes[0].classList.add('stayinyourchest');
+
+        
+            Array.from(promoboxes).forEach( function(el) {
+                
+                setTimeout(function() {
+                    el.classList.add('stayup');
+                },1000);
+                setTimeout(function() {
+                    el.classList.add('stayinyourchest');
+                    el.classList.remove('stayup');
+                },11000);
+            });
+       
     }
-});
+    
+
+})
