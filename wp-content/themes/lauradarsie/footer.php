0ee4ade5b2094a055959ccd1e9086880a6e59f8b
imeuro <mauro.fioravanzi@gmail.com>
2024-10-21 12:40:02 +0200

    link pagina info

--- wp-content/themes/lauradarsie/footer.php
+++ wp-content/themes/lauradarsie/footer.php
@@ -14,7 +14,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<small>Copyright &copy; <?php echo date("Y"); ?> - Laura Darsié - Tutti i diritti riservati - <a href="/privacy-policy/" class="privacylink">Content, Privacy e Cookie Policy</a></small>
+			<small>Copyright 2018-&copy; <?php echo date("Y"); ?> - Laura Darsié - Tutti i diritti riservati - <a href="/informazioni/" class="privacylink">Informazioni sui contenuti del sito</a> - <a href="/privacy-policy/" class="privacylink"Privacy e Cookie Policy</a></small>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 
